c9e6439445afde5be4f9defee7997267d3ef7c46
RCLU30 <15187974+RCLU30@users.noreply.github.com>
2019-04-12 13:07:15 -0400

    add authentication

---
+++ models/Users.js
@@ -0,0 +1,8 @@
+// mongoose model class
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const userSchema = new Schema({
+	googleId: String
+});
+
+mongoose.model('users', userSchema);
---
+++ routes/authRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const passport = require('passport')
+
+module.exports = (app) => {
+	// when user hits route, authenticate with password google oath
+	app.get('/auth/google', passport.authenticate('google', {
+		scope: ['profile', 'email']
+	}));
+
+	app.get('/auth/google/callback', passport.authenticate('google'));
+
+	app.get('/api/logout', (req, res) => {
+		req.logout();
+		res.send(req.user);
+	});
+
+	app.get('/api/current_user', (req, res) => {
+		res.send(req.user);
+	});
+}
+
