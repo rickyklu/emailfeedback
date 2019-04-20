3e9fb5518497efbb1b177337bb72dc7974e8e385
RCLU30 <15187974+RCLU30@users.noreply.github.com>
2019-04-20 13:06:03 -0400

    typo: clent => client

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV === 'production') {
 	// express serve index.html if non-express route(react routes)
 	const path = require('path');
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'clent', 'build', 'index.html'));
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
 }
 
